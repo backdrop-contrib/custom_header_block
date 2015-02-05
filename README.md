4d906c2fc540a1d85e1b1b063dd4cac22d5450cb
docwilmot <docwilmot@gmail.com>
2015-02-05 10:41:08 -0500

    Init

---
+++ README.md
@@ -0,0 +1,36 @@
+
+CUSTOM HEADER BLOCKS MODULE
+=================
+
+This module provides alternative configurations for site header components. 
+- Site logo
+- Site name
+- Site slogan
+
+The logo and site name are displayed inline (Site name to right of Logo, or a 
+flipped configuration with Site name to left), and with the site slogan either 
+or above the two inline components.
+Alternatively the default header arrangement can be used. However, this module 
+does not display the Account menu. This can of course be added as a separate 
+block.
+
+
+Installation
+------------
+1. Install this module using the official Backdrop CMS instructions at 
+https://backdropcms.org/guide/modules
+
+2. Add the header block to your Layout.
+
+3. Configure the header block and choose and save the desired configuration.
+
+License
+-------
+
+This project is GPL v2 software. See the LICENSE.txt file in this directory for
+complete text.
+
+Maintainers
+-----------
+
+- Andy Shillingford (https://github.com/docwilmot/)
---
+++ templates/custom_header_block.tpl.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @file
+ * Display logo to left of site name
+ *
+ * Available variables:
+ *
+ * - $base_path: The base URL path of the Backdrop installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $front_page: The URL of the front page. Use this instead of $base_path, when
+ *   linking to the front page. This includes the language domain or prefix.
+ * - $site_name: The name of the site, empty when display has been disabled.
+ * - $site_slogan: The site slogan, empty when display has been disabled.
+ */
+?>
+<?php if ($site_slogan && !$slogan_bottom): ?>
+  <div class="site-slogan slogan-top"><?php print $site_slogan; ?></div>
+<?php endif; ?>
+
+<?php if ($site_name || $logo): ?>
+  <div class="logo-and-name">
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="logo">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+    <?php endif; ?>
+    <?php if ($site_name): ?>
+      <?php if (!$is_front): ?>
+        <div class="site-name"><strong>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+        </strong></div>
+      <?php else: /* Use h1 when the content title is empty */ ?>
+        <h1 class="site-name">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+        </h1>
+      <?php endif; ?>
+    <?php endif; ?>
+  </div> <!-- /#logo-and-name -->
+<?php endif; ?>
+
+<?php if ($site_slogan && $slogan_bottom): ?>
+  <div class="site-slogan slogan-bottom"><?php print $site_slogan; ?></div>
+<?php endif; ?>
